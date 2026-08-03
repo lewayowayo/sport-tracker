98d90956ceb0cfb5f4308207915b2dd32b68b69d
wayowayo <contact@wayowayo.fr>
2026-08-03 19:17:50 +0200

    Tractions v2 : recalibrage depart bras tendus + objectifs dynamiques + fix cache SW

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
-const CACHE='sport-v3';
+const CACHE='sport-v4';
 const FILES=['./index.html','./manifest.json'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cl=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cl));return resp}))));
+self.addEventListener('fetch',e=>{
+  const isHTML=e.request.mode==='navigate'||e.request.url.indexOf('index.html')>-1;
+  if(isHTML){
+    e.respondWith(fetch(e.request).then(resp=>{const cl=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cl));return resp}).catch(()=>caches.match(e.request)));
+  }else{
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cl=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cl));return resp})));
+  }
+});
